37916d166602b8aed3531f8a92317a18fda36989
Daniel Duarte <danielduarte777@hotmail.com>
2018-12-18 14:13:28 +0000

    a
    a

---
+++ The Ostrich Survival/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    Transform player;
+    NavMeshAgent nav;
+    // Use this for initialization
+    void Start()
+    {
+        //MUDAR PARA PLAYER
+        player = GameObject.FindGameObjectWithTag("Zebra").transform;
+        nav = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        nav.SetDestination(player.position);
+    }
+}
---
+++ The Ostrich Survival/Assets/Scripts/NeutralMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NeutralMovement : MonoBehaviour
+{
+
+    Transform elephant;
+    NavMeshAgent nav;
+    // Use this for initialization
+    void Start()
+    {
+       
+        
+        nav = GetComponent<NavMeshAgent>();
+        elephant = GameObject.FindGameObjectWithTag("Elephant").transform;
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Random.Range(0, 200);
+        var z = Random.Range(0, 200);
+
+
+        Vector3 vector0 = new Vector3();
+        vector0 = elephant.position;
+        Vector3 vector = new Vector3(200, vector0.y, 200);
+        nav.SetDestination(vector);
+    }
+}
